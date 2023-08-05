a0b771e94160946da04875c67be3c27e6fcbf2e6
Geier Csaba <geier.csaba@gmail.com>
2023-08-05 20:41:07 +0200

    aldi db eltávolítás, visszajelzés

--- merge_db.py
+++ merge_db.py
@@ -1,41 +1,45 @@
 import sqlite3
 
-
-def db_merge():
-    # Adatbáziskapcsolat létrehozása
-    # Új adatbázis hozzáadásához csak bővíteni kell a databases tömböt
-    databases = ["tesco", "auchan", "aldi"]
-    merged_conn = sqlite3.connect('merged_db.db')
-    merged_cursor = merged_conn.cursor()
-
+def create_tables(merged_cursor, merged_conn, databases):
     # Új tábla létrehozása a termékeknek és az áraknak a merged_db-ben
     columns = ['name', 'category']
     columns.extend(databases)
     merged_cursor.execute(f'''
-        CREATE TABLE IF NOT EXISTS products (
-            id INTEGER PRIMARY KEY,
-            {", ".join(f"{col} TEXT" for col in columns)}
-        )
-    ''')
+            CREATE TABLE IF NOT EXISTS products (
+                id INTEGER PRIMARY KEY,
+                {", ".join(f"{col} TEXT" for col in columns)}
+            )
+        ''')
     merged_cursor.execute('''
-        CREATE TABLE IF NOT EXISTS prices (
-            date INTEGER DEFAULT (strftime('%s', 'now')),
-            normal_price INTEGER,
-            discount_price INTEGER,
-            product_sku TEXT,
-            product_id INT,
-            FOREIGN KEY (product_id) REFERENCES products(id)
-        )
-    ''')
+            CREATE TABLE IF NOT EXISTS prices (
+                date INTEGER DEFAULT (strftime('%s', 'now')),
+                normal_price INTEGER,
+                discount_price INTEGER,
+                product_sku TEXT,
+                product_id INT,
+                FOREIGN KEY (product_id) REFERENCES products(id)
+            )
+        ''')
     merged_conn.commit()
+    print("adatbázis létrehozva")
 
+
+def db_merge():
+    # Adatbáziskapcsolat létrehozása
+    # Új adatbázis hozzáadásához csak bővíteni kell a databases tömböt
+    databases = ["tesco", "auchan"]
+    merged_conn = sqlite3.connect('databases/merged_db.db')
+    merged_cursor = merged_conn.cursor()
+
+    # Új tábla létrehozása a termékeknek és az áraknak a merged_db-ben
+    create_tables(merged_cursor, merged_conn, databases)
     # Adatbázisok összekapcsolása
-    for db in databases:
-        db_conn = sqlite3.connect(f'adatbazis_{db}.db')
+    for db in databases[0:0]:
+        db_conn = sqlite3.connect(f'databases/adatbazis_{db}.db')
         db_cursor = db_conn.cursor()
 
         # Összekapcsolás a merged_db products táblával a név alapján
-        db_cursor.execute('SELECT (sku, name, category) FROM products')
+        db_cursor.execute('SELECT sku, name, category FROM products')
         products = db_cursor.fetchall()
         for sku, name, category in products:
             merged_cursor.execute('''
@@ -66,6 +70,8 @@ def db_merge():
 
         db_conn.close()
         merged_conn.commit()
+        print(f"adatbázis -{db}- sikeresen hozzáadva")
+
 
     # Adatbázis lezárása
     merged_conn.close()
